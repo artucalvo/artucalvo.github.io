e8df945871269d4d5058a81c9511fc8607f1d5e8
Arturo Calvo Devesa <arturocalvodevesa@gmail.com>
2014-11-30 17:07:38 +0000

    deleted:    js/contact_me.js~

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -8,17 +8,35 @@ if(empty($_POST['name'])  		||
 	echo "No arguments Provided!";
 	return false;
    }
-	
+
 $name = $_POST['name'];
 $email_address = $_POST['email'];
 $message = $_POST['message'];
-	
-// Create the email and send the message
-$to = 'hello@arturocalvo.com';
-$email_subject = "Website Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nMessage:\n$message";
-$headers = "From: hello@arturocalvo.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";	
-mail($to,$email_subject,$email_body,$headers);
+
+require 'PHPMailerAutoload.php';
+$mail = new PHPMailer;
+
+$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->Host = 'smtp.gmail.com';                       // Specify main and backup server
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = 'arturocalvodevesa@gmail.com';      // SMTP username
+$mail->Password = '4N72asG9';               	      // SMTP password
+$mail->SMTPSecure = 'tls';                            // Enable encryption, 'ssl' also accepted
+$mail->Port = 587;                                    //Set the SMTP port number - 587 for authenticated TLS
+$mail->setFrom('arturocalvodevesa@gmail.com', 'Arturo Calvo');     //Set who the message is to be sent from
+$mail->addAddress('hello@arturocalvo.com', 'Arturo Calvo');  // Add a recipient
+$mail->isHTML(false);                                  // Set email format to HTML
+ 
+$mail->Subject = "Website Contact Form:  $name";
+$mail->Body    = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nMessage:\n$message";
+ 
+//Read an HTML message body from an external file, convert referenced images to embedded,
+//convert HTML into a basic plain-text alternative body
+$mail->msgHTML(file_get_contents('contents.html'), dirname(__FILE__));
+ 
+if(!$mail->send()) {
+   return false;
+}
+ 
 return true;			
 ?>
